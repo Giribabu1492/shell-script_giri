3fcb18884dd7c4910afd004e08a90aa17fd7e7d4
Giribabu1492 <jinagagiribabu@gmail.com>
2025-04-22 13:26:07 +0530

    commiting variables.sh file

--- 02-datatype.sh
+++ 02-datatype.sh
@@ -2,6 +2,6 @@
 
 movies=("pushpa" "ravi" "mahesh")
 
-echo "first moviename is $movies[0]"
-echo "second movienameis $movies[1]"
-echo "third moviename is  $movies[2]"
+echo "first moviename is ${movies[0]}"
+echo "second movienameis ${movies[1]}"
+echo "third moviename is  ${movies[2]}"
