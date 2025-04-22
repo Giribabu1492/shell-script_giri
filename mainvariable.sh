d3e990c42a4ca7f83f7b1fa2cb95d2b790766dd4
Giribabu1492 <jinagagiribabu@gmail.com>
2025-04-22 11:38:38 +0530

    coomiting to main

--- mainvariable.sh
+++ mainvariable.sh
@@ -4,8 +4,8 @@ persion1 ="ram"
 persion2 ="rahim"
 
 
-echo "$persion1: howr u? "
-echo "$persion2:i am good"
-echo "$persion1:what r u doing"
-echo "$persion2:i am doing well"
+echo "$persion1:: howr u? "
+echo "$persion2::i am good"
+echo "$persion1::what r u doing"
+echo "$persion2::i am doing well"
 
