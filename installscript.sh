77808fd4a9474377042c6bf3308d2887fb2a71d0
Giribabu1492 <jinagagiribabu@gmail.com>
2025-04-22 18:42:27 +0530

    commiting variables.sh file

--- installscript.sh
+++ installscript.sh
@@ -8,5 +8,30 @@ echo "error you should have root access"
 
 fi
   
-dnf install mysql -y
+dnf install mysqll -y
+
+if [ $? ne 0]
+then
+echo "installing mysql.....failure"
+
+else 
+
+echo "installing mysql..success"
+
+fi
+
+dnf install git -y
+
+if [ $? ne 0]
+then
+echo "installing git.....failure"
+
+else 
+
+echo "installing git..success"
+
+fi
+
+
+
 
