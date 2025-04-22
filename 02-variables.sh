c790884cacbece25c98be9ca17f062f8415571df
Giribabu1492 <jinagagiribabu@gmail.com>
2025-04-22 12:16:53 +0530

    commiting variables.sh file

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+persion1=$1 #no spaces between variables
+persion2=$2
+
+echo "$persion1:: HI How r u?"
+echo "$persion2: i am good buddy.."
+echo  "$persion1 :: what r u doing??"
+echo "$persion2:: i am working in office"
